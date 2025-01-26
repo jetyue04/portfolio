378ec661968f5056a0f54213757d52121258d29e
jetyue04 <66549989+jetyue04@users.noreply.github.com>
2025-01-25 23:36:49 -0800

    Update global.js

--- global.js
+++ global.js
@@ -4,13 +4,6 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// let navliinks = $$('nav a');
-// let currentLink = navLinks.find(
-//     (a) => a.host === location.host && a.pathname === location.pathname
-//   );
-
-// currentLink?.classList.add('current');
-
 let pages = [
     { url: '/portfolio/', title: 'Home' },
     { url: '/portfolio/projects/', title: 'Projects' },
@@ -26,15 +19,17 @@ for (let p of pages) {
     let url = p.url;
     let title = p.title;
     const ARE_WE_HOME = document.documentElement.classList.contains('home');
+    
     if (!ARE_WE_HOME && !url.startsWith('http')) {
         url = '../' + url; 
     }
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 
-    // let a = document.createElement('a');
-    // a.href = url;
-    // a.textContent = title;
-    // nav.append(a);
+    // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
 
     if (a.host === location.host && a.pathname === location.pathname) {
         a.classList.add('current');
